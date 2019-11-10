a817a95edd1adca61529f69471a4829ab1c3d3cb
gitudrebel <gitudrebel94@gmail.com>
2019-11-10 20:55:01 +0530

    Display ads

--- app/src/main/java/com/droidmonk/exodemo/MainActivity.kt
+++ app/src/main/java/com/droidmonk/exodemo/MainActivity.kt
@@ -5,20 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.ima.ImaAdsLoader
+import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.ads.AdsMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var player: SimpleExoPlayer? = null
+    private var adsLoader: ImaAdsLoader? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adsLoader = ImaAdsLoader(this, Uri.parse(resources.getString(R.string.ad_tag)))
     }
 
     override fun onStart() {
@@ -26,16 +30,31 @@ class MainActivity : AppCompatActivity() {
 
         player=ExoPlayerFactory.newSimpleInstance(this,DefaultTrackSelector())
         player_view.player=player
+        adsLoader?.setPlayer(player)
 
 
         val dataSourceFactory = DefaultDataSourceFactory(
             this,
             "ExoDemo")
 
-        val mediaSource:MediaSource=ProgressiveMediaSource.Factory(dataSourceFactory)
+        val mediaSource:MediaSource=ExtractorMediaSource.Factory(dataSourceFactory)
             .createMediaSource(Uri.parse(resources.getString(R.string.media_url_mp4)))
 
-        player?.prepare(mediaSource)
+        val adsMediaSource = AdsMediaSource(mediaSource, dataSourceFactory, adsLoader, player_view)
+
+
+        player?.prepare(adsMediaSource)
         player?.setPlayWhenReady(true)
+    }
+
+
+    override fun onStop() {
+
+        adsLoader?.setPlayer(null)
+        player_view.setPlayer(null)
+        player?.release()
+        player = null
+
+        super.onStop()
     }
 }
