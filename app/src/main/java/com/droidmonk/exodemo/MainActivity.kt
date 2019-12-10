f8742884efe116e96f2ae0152b98ed9450e84d10
gitudrebel <gitudrebel94@gmail.com>
2019-12-10 23:32:26 +0530

    release resources

--- app/src/main/java/com/droidmonk/exodemo/MainActivity.kt
+++ app/src/main/java/com/droidmonk/exodemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.droidmonk.exodemo
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -13,6 +14,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG:String="MainActivity"
+
+    private var currentPosition: Long = 0
     private var player: SimpleExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +38,32 @@ class MainActivity : AppCompatActivity() {
         val mediaSource:MediaSource=ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(Uri.parse(resources.getString(R.string.video_url)))
 
-
-
         player?.prepare(mediaSource)
+        player?.seekTo(currentPosition)
         player?.setPlayWhenReady(true)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG,"Onstop")
+
+        currentPosition = player!!.contentPosition
+        player_view.setPlayer(null)
+        player!!.release()
+        player = null
+
+        super.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"Onpause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"Onresume")
     }
 }
