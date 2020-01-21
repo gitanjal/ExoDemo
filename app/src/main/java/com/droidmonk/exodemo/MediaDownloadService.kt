3cab335c4c6b5083eca7127ec9b4cac8b3f4cc8e
gitudrebel <gitudrebel94@gmail.com>
2020-01-21 15:49:14 +0530

    Add download service notification channel desc

--- app/src/main/java/com/droidmonk/exodemo/MediaDownloadService.kt
+++ app/src/main/java/com/droidmonk/exodemo/MediaDownloadService.kt
@@ -4,23 +4,18 @@ import android.app.Notification
 import com.google.android.exoplayer2.offline.Download
 import com.google.android.exoplayer2.offline.DownloadManager
 import com.google.android.exoplayer2.offline.DownloadService
-import com.google.android.exoplayer2.scheduler.PlatformScheduler
 import com.google.android.exoplayer2.scheduler.Scheduler
 import com.google.android.exoplayer2.ui.DownloadNotificationHelper
-import com.google.android.exoplayer2.util.Util
 
 class MediaDownloadService() : DownloadService(FOREGROUND_NOTIFICATION_ID,
     DEFAULT_FOREGROUND_NOTIFICATION_UPDATE_INTERVAL,
     CHANNEL_ID,
-    R.string.exo_download_notification_channel_name,
-    0) {
+    R.string.download_service_notification_channel,
+    R.string.download_service_notification_channel_description) {
 
     companion object {
         private const val CHANNEL_ID = "download_channel"
-        private const val JOB_ID = 1
         private const val FOREGROUND_NOTIFICATION_ID = 1
-        private var nextNotificationId =
-            FOREGROUND_NOTIFICATION_ID + 1
     }
 
     lateinit var notificationHelper: DownloadNotificationHelper
